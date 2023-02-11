e363ca1a7b13f7ca5418197bc4214911ae0ab8f3
Osama Bin Hashim <zeeshan2k01@gmail.com>
2023-02-11 19:25:39 +0600

    signup data parsing

--- app/src/main/java/com/abedkhan/multimedia/Fragment/SignUpFragmentOne.java
+++ app/src/main/java/com/abedkhan/multimedia/Fragment/SignUpFragmentOne.java
@@ -3,12 +3,18 @@ package com.abedkhan.multimedia.Fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.abedkhan.multimedia.R;
+import com.abedkhan.multimedia.databinding.FragmentSignUpOneBinding;
 
 
 public class SignUpFragmentOne extends Fragment {
@@ -16,10 +22,58 @@ public class SignUpFragmentOne extends Fragment {
         // Required empty public constructor
     }
 
+    FragmentSignUpOneBinding binding;
+    String fullName, userName, email;
+    String selectedGender="";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up_one, container, false);
+        binding = FragmentSignUpOneBinding.inflate(getLayoutInflater(), container, false);
+
+//        Gender radio group
+        binding.genderRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int id) {
+                RadioButton genderButton = getActivity().findViewById(id);
+                selectedGender = genderButton.getText().toString();
+            }
+        });
+
+//        handling next button clicked
+        binding.nextButton.setOnClickListener(view -> {
+            fullName = binding.signupFullName.getText().toString().trim();
+            userName = binding.signupUsername.getText().toString().trim();
+            email = binding.signupEmail.getText().toString().trim();
+
+            if (fullName.isEmpty()){
+                binding.signupFullName.setError("Field cannot be empty!");
+            } else if (userName.isEmpty()) {
+                binding.signupUsername.setError("Field cannot be empty!");
+            } else if (email.isEmpty()) {
+                binding.signupEmail.setError("Field cannot be empty!");
+            } else if (selectedGender.isEmpty()) {
+                Toast.makeText(getActivity(), "Please select a gender!", Toast.LENGTH_SHORT).show();
+            } else {
+                Fragment fragment = new SignUpFragmentTwo();
+//                passing data to fragment
+                Bundle bundle = new Bundle();
+                bundle.putString("Signup_FullName", fullName);
+                bundle.putString("Signup_UserName", userName);
+                bundle.putString("Signup_Email", email);
+                bundle.putString("Signup_Gender", selectedGender);
+                fragment.setArguments(bundle);
+
+
+//            --going to the signup page two
+
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.containerFrame, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        return binding.getRoot();
     }
 }
--- app/src/main/java/com/abedkhan/multimedia/Fragment/SignUpFragmentTwo.java
+++ app/src/main/java/com/abedkhan/multimedia/Fragment/SignUpFragmentTwo.java
@@ -3,12 +3,15 @@ package com.abedkhan.multimedia.Fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.abedkhan.multimedia.R;
+import com.abedkhan.multimedia.databinding.FragmentSignUpTwoBinding;
 
 public class SignUpFragmentTwo extends Fragment {
 
@@ -17,10 +20,33 @@ public class SignUpFragmentTwo extends Fragment {
     }
 
 
+    FragmentSignUpTwoBinding binding;
+    String fullName, userName, email, gender;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up_two, container, false);
+        binding = FragmentSignUpTwoBinding.inflate(getLayoutInflater(), container, false);
+
+//        getting data from the fragment
+        fullName = getArguments().getString("Signup_FullName");
+        userName = getArguments().getString("Signup_UserName");
+        email = getArguments().getString("Signup_Email");
+        gender = getArguments().getString("Signup_Gender");
+
+        Log.i("TAG", "Signup two:-> "+fullName+"\nUserName: "+userName+"\nEmail: "+email+"\nGender:"+ gender);
+
+//        Back Button handling
+        binding.backBtn.setOnClickListener(view -> {
+            //            --going to the signup page two
+            Fragment fragment = new SignUpFragmentOne();
+
+            FragmentTransaction transaction = getActivity().getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.containerFrame, fragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        });
+
+        return binding.getRoot();
     }
 }

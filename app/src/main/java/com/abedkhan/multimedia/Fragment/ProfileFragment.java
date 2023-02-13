637a2d9d3df5fc80b54799c5b0c2f02e977551d7
ar-abedkhan <abedkhan783@gmail.com>
2023-02-14 02:18:24 +0600

    floating button

--- app/src/main/java/com/abedkhan/multimedia/Fragment/ProfileFragment.java
+++ app/src/main/java/com/abedkhan/multimedia/Fragment/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.abedkhan.multimedia.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,16 +9,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.abedkhan.multimedia.Activities.MainActivity;
+import com.abedkhan.multimedia.Activities.ShoppingMainActivity;
 import com.abedkhan.multimedia.R;
 import com.abedkhan.multimedia.databinding.FragmentProfileBinding;
+import com.github.clans.fab.FloatingActionButton;
 
 public class ProfileFragment extends Fragment {
-
     public ProfileFragment() {
     }
 
-    FragmentProfileBinding binding;
-
+    private FragmentProfileBinding binding;
 
 
 
@@ -25,6 +27,16 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 //        binding=FragmentProfileBinding.inflate(getLayoutInflater(),container,false);
+        FloatingActionButton floatingActionButton;
+//
+//floatingActionButton=container.findViewById(R.id.menu_item);
+//
+//
+//
+//       floatingActionButton.setOnClickListener(view -> {
+//            startActivity(new Intent(requireContext(),ShoppingMainActivity.class));
+//        });
+////
         return inflater.inflate(R.layout.fragment_profile, container, false);
 //        return binding.getRoot();
     }
